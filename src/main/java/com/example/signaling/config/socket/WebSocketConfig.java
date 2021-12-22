a04107bcdc72afbf4aa991438a10e47be73fdc3e
Heedong Kim <ruthetum@gmail.com>
2021-12-22 15:52:41 +0900

    webrtc + 메세지

---
+++ src/main/java/com/example/signaling/config/socket/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.example.signaling.config.socket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    /**
+     * 클라이언트에서 등록할 URL : /socket
+     * add handler로 SocketHandler 전달
+     */
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/socket")
+                .setAllowedOrigins("*");
+    }
+}
